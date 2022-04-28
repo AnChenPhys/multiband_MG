fixing derivatives

code/utilities_simple.py
@@ -335,9 +335,13 @@ def Psi_Delta_exact(fo, fstar, c0, Mz, eta, zem, cosmo, tc, psic, cT_type='EFT',
 # distance correlation term in the phase
 def Psi_dist_corr(fo, fstar, c0, zem, cosmo_params):
 
-    dist_val =  dcom(zem, fo, cosmo_params)
+    result = np.zeros(len(fo))
+    for i in range(len(fo)):
+        dist_val =  dcom(zem, fo[i], cosmo_params)
 
-    return 2*np.pi*fo * Mpc *dist_val/c*(1-c0)*(1-fstar/fo)*np.heaviside(fstar-fo,1)
+        result[i] = 2*np.pi*fo[i] * Mpc *dist_val/c*(1-c0)*(1-fstar/fo[i])*np.heaviside(fstar-fo[i],1)
+
+    return result
 
 # amplitude for exact Delta
 def amp_Delta_exact(fo, fstar, c0, Mz, eta, zem, cosmo_params, cT_type='EFT', width=0):